876c64e3af94be7dd7af2c94a04c0fd0bb96cfe5
Laissa Fernandes <laissafermaria@gmail.com>
2022-08-12 14:03:49 -0300

    Modificando endpoint

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -7,56 +7,70 @@ import Pokeinfo from "./Pokeinfo";
 import { useState } from "react";
 import { useEffect } from "react";
 
-function Main(){
-    const[pokeData, setPokeData]=useState([]);
-    const[loading,setLoading]=useState(true);
-    const[url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/");
-    const[nextUrl, setNextUrl]=useState();
-    const [prevUrl, setPrevUrl]=useState();
-    const[pokeDex,setPokeDex]=useState();
-    const pokeFun=async()=>{
-        setLoading(true)
-        const res=await axios.get(url);
-        //console.log(res.data.results);
-        setNextUrl(res.data.next);
-        setPrevUrl(res.data.previous);
-        getPokemon(res.data.results);
-        setLoading(false);
-        //console.log(pokeData);
+function Main() {
+    const [pokeData, setPokeData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/?limit=5&offset=5");
+    const [nextUrl, setNextUrl] = useState("");
+    const [prevUrl, setPrevUrl] = useState("");
+    const [pokeDex, setPokeDex] = useState();
 
-    }
-    const getPokemon = async(res)=>{
-        res.map(async(item) => {
-            const result=await axios.get(item.url)
-            //console.log(result.data);
-            setPokeData(state=>{
-                state=[...state,result.data];
-                state.sort((a,b)=>a.id>b.id?1:+1) 
+    const getPokemon = async (res) => {
+        // tirar o async
+        // const test =// 
+        res.map(async (item) => {
+        
+            const result = await axios.get(item.url)
+            // return{
+            //     ...item,
+            //     data: result.data
+            // }
+            
+
+            setPokeData(state => {
+                state = [...state, result.data];
+                state.sort((a, b) => a.id > b.id ? 1 : -1)
                 return state;
             })
 
 
         });
+        // setPokeData(test)
+        // console.log(test)
+       
+
     }
-    useEffect(()=>{
+
+    useEffect(() => {
+        const pokeFun = async () => {
+            setLoading(true)
+            const res = await axios.get(url);
+            
+            setNextUrl(res.data.next);
+            setPrevUrl(res.data.previous);
+            getPokemon(res.data.results);
+            setLoading(false);
+            
+
+        }
         pokeFun();
     }, [url]);
 
     return (
 
-        
+
         <div className={styles.row}>
             <div className={styles.card} >
                 <h2>Pokemons</h2>
-                <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)} />
-               
+                <Card pokemon={pokeData} loading={loading} infoPokemon={poke => setPokeDex(poke)} />
+
                 <div className={styles.container_btn}>
-                    { prevUrl && <Button text="Voltar" onClick={()=>{
+                    {prevUrl && <Button text="Voltar" onClick={() => {
                         setPokeData([])
                         setUrl(prevUrl)
                     }} />}
 
-                    {nextUrl && <Button text="Avançar" onClick={()=>{
+                    {nextUrl && <Button text="Avançar" onClick={() => {
                         setPokeData([])
                         setUrl(nextUrl)
                     }} />}
@@ -65,7 +79,7 @@ function Main(){
 
             <div className={styles.card}>
                 <Pokeinfo data={pokeDex} />
-                
+
             </div>
 
         </div>
